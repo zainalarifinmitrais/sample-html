761a9ff891edc524be5f271961845fabdf5749ea
rfajarachmad <achmadfajar.ridwan@mitrais.com>
2017-02-03 15:52:26 +0700

    no message

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
 	def applicationName = 'fajar-app' //change me
-	def deploymentGroupName = 'fajar-deployment-group' // change me
+	def deploymentGroupName = 'fajar-app' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
